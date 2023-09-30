6c9a969cda6751fbc1ed082b62a685c13dc13df5
P-21710 <98374171+P-21710@users.noreply.github.com>
2023-09-30 23:22:56 +0530

    Complete Practical 5

--- app/src/main/java/com/prasadrajyaguru/mad_practical_5_21012011127/MainActivity.kt
+++ app/src/main/java/com/prasadrajyaguru/mad_practical_5_21012011127/MainActivity.kt
@@ -15,33 +15,75 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val buttonBrowse:Button=findViewById(R.id.)
-        val editUrl:EditText=findViewById(R.id.)
-        buttonBrowse.setOnClickListener { Onbrowse(editUrl.text.toString()) }
-    }
-    fun Onbrowse(Url:String)
-    {
-        Intent(Intent.ACTION_VIEW, Uri.parse(Url)).also { startActivity(it) }
-    }
-    fun Oncall(Number:String)
-    {
-        Intent(Intent.ACTION_DIAL, Uri.parse(Number)).also { startActivity(it) }
+
+        val buttonBrowse: Button = findViewById(R.id.btnBrowse)
+        val editUrl : EditText = findViewById(R.id.EditTextUrl)
+
+        buttonBrowse.setOnClickListener()
+        {
+            onBrowse(editUrl.text.toString())
+        }
+
+        val buttonCall: Button = findViewById(R.id.btnCall)
+        val callNumber : EditText = findViewById(R.id.EditTextNumber )
+
+        buttonCall.setOnClickListener()
+        {
+            onCall(callNumber.text.toString())
+        }
+
+        val buttonCallLog: Button = findViewById(R.id.btnCallLog)
+
+        buttonCallLog.setOnClickListener()
+        {
+            onCallLog()
+        }
+
+        val buttonGallery: Button = findViewById(R.id.btnGallery)
+
+        buttonGallery.setOnClickListener()
+        {
+            onGallery()
+        }
+
+        val buttonCamera: Button = findViewById(R.id.btnCamera)
+
+        buttonCamera.setOnClickListener()
+        {
+            onCamera()
+        }
+
+        val buttonAlarm: Button = findViewById(R.id.btnAlarm)
+
+        buttonAlarm.setOnClickListener()
+        {
+            onAlarm()
+        }
 
     }
-    fun Oncalllog()
+    fun onBrowse(url: String)
+    {
+        Intent(Intent.ACTION_VIEW, Uri.parse(url)).also{startActivity(it) }
+    }
+    fun onCall(number:String)
+    {
+        Intent(Intent.ACTION_DIAL).setData(Uri.parse("tel:$number")).also { startActivity(it) }
+    }
+    fun onCallLog()
     {
         Intent(Intent.ACTION_VIEW).setType(CallLog.Calls.CONTENT_TYPE).also { startActivity(it) }
     }
-    fun Gallery()
+    fun onGallery()
     {
-        Intent(Intent.ACTION_VIEW).setType("Image").also { startActivity(it) }
+        Intent(Intent.ACTION_VIEW).setType("image/*").also { startActivity(it) }
     }
-    fun Oncamera()
+    fun onCamera()
     {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also {startActivity(it) }
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { startActivity(it) }
     }
-    fun Alarm()
+    fun onAlarm()
     {
         Intent(AlarmClock.ACTION_SHOW_ALARMS).also { startActivity(it) }
     }
+
 }
